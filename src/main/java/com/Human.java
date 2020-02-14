245d980582321a74c22a6c06aaf21a3f4fc0137e
sesanp4@gmail.com <sesanp4@gmail.com>
2020-02-14 10:13:33 +0100

    first commit

---
+++ src/main/java/com/Human.java
@@ -0,0 +1,14 @@
+package com;
+
+import lombok.Data;
+
+@Data
+
+public class Human {
+
+    String name;
+
+    String age;
+
+
+}
---
+++ src/main/java/com/TestHuman.java
@@ -0,0 +1,19 @@
+package com;
+
+public class TestHuman {
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("ssp");
+        human.setAge("20");
+        System.out.println(human.toString());
+        //log.info("");
+
+    }
+
+
+    public void test(){
+
+    }
+
+}
